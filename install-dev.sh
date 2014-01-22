de220e1d4b57845885855878243497a3639b9e78
dr <dr@webfactory.de>
2014-01-22 19:27:46 +0100

    Einfache Install-Skripte hinzugefuegt.

---
+++ install-dev.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+WWW_USER=www
+WWW_GROUP=www
+
+
+#### Composer
+if [ -f /opt/local/share/composer/bin/composer ];
+then
+    COMPOSER_EXECUTABLE=/opt/local/share/composer/bin/composer
+else
+    COMPOSER_EXECUTABLE=$(which composer)
+fi
+
+sudo $COMPOSER_EXECUTABLE install
+
+### Node Package Manager
+npm install
+
+
+### Bower
+bower install
+
+### Grunt
+grunt build
+
+
+### Bootstrap-Caching
+sudo touch app/bootstrap.php.cache
+
+sudo chown -R $WWW_USER:$WWW_GROUP app/bootstrap.php.cache
+sudo chown -R $WWW_USER:$WWW_GROUP app/cache
+sudo chown -R $WWW_USER:$WWW_GROUP app/logs
+
+
