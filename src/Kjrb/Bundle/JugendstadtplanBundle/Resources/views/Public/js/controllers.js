869c5c9733b6f835e110eebcdb8143dff06c8c96
dr <dr@webfactory.de>
2014-01-26 00:13:28 +0100

    Auf der Detailseite eines Ortes _nur_ den Marker des Ortes anzeigen.

--- src/Kjrb/Bundle/JugendstadtplanBundle/Resources/views/Public/js/controllers.js
+++ src/Kjrb/Bundle/JugendstadtplanBundle/Resources/views/Public/js/controllers.js
@@ -50,3 +50,25 @@ app.controller("OrtErstellenController", [ '$scope', function($scope) {
         }
     });
 }]);
+
+app.controller("OrtDetailseiteController", [ '$scope', function($scope) {
+
+    angular.extend($scope, {
+        center: {
+            lat: 50.732829246726, // Default-Werte, werden vom Model ueberschrieben.
+            lng: 7.0937004090117, // Default-Werte, werden vom Model ueberschrieben.
+            zoom: 15
+        },
+        defaults: {
+            scrollWheelZoom: false
+        },
+        markers: {
+            default_marker: {
+                lat: 50.732829246726,
+                lng: 7.0937004090117,
+                message: "",
+                focus: true
+            }
+        }
+    });
+}]);
