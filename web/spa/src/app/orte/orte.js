fc42c5a3ef96badee9ec1391f7e270e2881c355c
dr <dr@webfactory.de>
2014-02-16 15:20:12 +0100

    Views besser organisiert + Ausgabe einer Detailseite.

--- web/spa/src/app/orte/orte.js
+++ web/spa/src/app/orte/orte.js
@@ -10,16 +10,41 @@ angular.module( 'ngBoilerplate.orte', [
     views: {
       "main": {
         controller: 'OrteController',
-        templateUrl: 'orte/orte.tpl.html'
+        templateUrl: 'orte/views/orte.tpl.html'
       }
     },
     data:{ pageTitle: 'Orte' }
-  });
+  })
+  .state( 'detail', {
+    url: '/ort/:id',
+    views: {
+      "main": {
+        controller: 'OrtDetailController',
+        templateUrl: 'orte/views/detail.tpl.html'
+      }
+    },
+    resolve: {
+      ortDetail: [ '$stateParams', 'OrteApi', function($stateParams, $orteApi) {
+        return $orteApi.get({},{id: $stateParams.id});
+      }]
+    },
+    data:{ pageTitle: 'Ort' }
+  })
+  ;
+
 
 })
 
-.controller( 'OrteController', [ '$scope', 'OrteApi', function OrteController( $scope, $orteApi ) {
+.controller( 'OrteController', [ '$scope', '$location', 'OrteApi', function OrteController( $scope, $location, $orteApi ) {
     $scope.orte = $orteApi.query();
+
+    $scope.viewOrt = function (ort) {
+      $location.path('/ort/'+ort.id);
+    };
+
 }])
 
+.controller( 'OrtDetailController', [ '$scope', 'ortDetail', function OrtDetailController( $scope, ortDetail ) {
+    $scope.ort = ortDetail;
+}])
 ;
