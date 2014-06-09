c01474883733cb3ec37f588ac462ab628ad59057
dr <dr@webfactory.de>
2014-06-09 22:17:03 +0200

    Auf der OrtDetailseite wird nun eine Mini-Karte mit dem zugehoerigen Ort eingeblendet.

--- frontend/src/app/orte/OrteDetailController.js
+++ frontend/src/app/orte/OrteDetailController.js
@@ -20,4 +20,39 @@ angular.module('jugendstadtplan.orte').config(function config( $stateProvider ) 
 
 Jugendstadtplan.Controllers.controller( 'OrtDetailController', [ '$scope', 'ortDetail', function OrtDetailController( $scope, ortDetail ) {
     $scope.ort = ortDetail;
+    $scope.markers = [];
+
+    ortDetail.$promise.then(function() {
+            angular.extend($scope, {
+                center: {
+                    lat: ortDetail.latitude,
+                    lng: ortDetail.longitude,
+                    zoom: 16
+                },
+                defaults: {
+                    scrollWheelZoom: false
+                }
+            });
+
+            var marker = {
+                lat: ortDetail.latitude,
+                lng: ortDetail.longitude,
+                title: ortDetail.titel,
+                focus: true,
+                message: '<h3>' + ortDetail.titel + '</h3>' + ortDetail.beschreibung
+            };
+            $scope.markers.push(marker);
+        }, function() {
+            angular.extend($scope, {
+                center: {
+                    lat: 50.732829246726,
+                    lng: 7.0937004090117,
+                    zoom: 13
+                },
+                defaults: {
+                    scrollWheelZoom: false
+                }
+            });
+        }
+    );
 }]);
