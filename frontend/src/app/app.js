7f7ad01d48e98f24dd7b74fade729bf839cf8718
dr <dr@webfactory.de>
2014-07-10 20:20:26 +0200

    Code robuster gegenueber Minification gemacht..

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -23,12 +23,12 @@ angular.module( 'ngBoilerplate', [
 .run( function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+.controller( 'AppCtrl', [ '$scope', '$location', function AppCtrl( $scope, $location ){
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | Jugendstadtplan' ;
     }
   });
-})
+}])
 
 ;
