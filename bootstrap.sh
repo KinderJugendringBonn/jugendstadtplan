ece22f0a8ec82bd6cfdab5c41f9181b73f22a0b7
Dominik Ritter <dr@webfactory.de>
2014-11-18 23:59:29 +0100

    Provisionierung fuer die Vagrant-Box verbessert..

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,22 @@ gem list
 sudo rm -fr /etc/apache2/sites-enabled/000-default.conf
 sudo ln -sf /var/www/conf/apache-vagrant.conf /etc/apache2/sites-enabled/010-jugendstadtplan.conf
 
-cd /var/www && composer install
+# Backend installieren
+cd /var/www 
+composer install
+php ./vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php
 php app/console doctrine:database:create -q
 php app/console doctrine:schema:update --force
-cd /var/www/frontend && npm install && bower install && grunt -f build
+
+# Dateirecht fuer npm korrekt setzen
+sudo chown -R vagrant:vagrant /home/vagrant/.npm
+
+# Frontend installieren
+cd /var/www/frontend
+mkdir build
+npm install
+bower install
+grunt -f build
 
 ## mod_proxy aktivieren, damit die Rewrites fuer das Backend funktionieren
 sudo a2enmod proxy
