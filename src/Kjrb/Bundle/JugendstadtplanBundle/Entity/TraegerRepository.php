570c33b0e87ae3f39c9b26d7ac5596a3c726de3a
Dominik Ritter <dritter03@googlemail.com>
2016-03-28 22:29:04 +0200

    Whitespace entfernt

--- src/Kjrb/Bundle/JugendstadtplanBundle/Entity/TraegerRepository.php
+++ src/Kjrb/Bundle/JugendstadtplanBundle/Entity/TraegerRepository.php
@@ -5,10 +5,9 @@ namespace Kjrb\Bundle\JugendstadtplanBundle\Entity;
 use Doctrine\ORM\EntityRepository;
 
 class TraegerRepository extends EntityRepository {
-    
+
     public function findAllActivated() {
         return $this->findBy(array('isActive' => true));
     }
 
 }
- 
